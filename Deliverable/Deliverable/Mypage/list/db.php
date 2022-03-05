7b5483967aa0d88c1a53f54877ea4a6c17aac2c8
kernelmurton <92277129+kernelmurton@users.noreply.github.com>
2022-03-06 01:31:59 +0900

    Delete Deliverable/Deliverable directory

--- Deliverable/Deliverable/Mypage/list/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "mysql634.db.sakura.ne.jp";
-$dbName = "surija_main";
-$user = "surija";
-$password = "zetton1207";
-$dsn = "mysql:host={$host};dbname={$dbName};charser=utf8";
-?>
--- Deliverable/Deliverable/main2/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$_SESSION = array();
-setcookie(session_name(), '', time()-1, '/');
-session_destroy();
-
-header('Location:./login.php');
-?>
